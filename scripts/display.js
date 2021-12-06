3440262674066640bb2a589162e74f4ca9424ed5
Yash-RajSingh <yashraj45202@gmail.com>
2021-12-07 01:06:39 +0530

    Completed Major parts

--- scripts/display.js
+++ scripts/display.js
@@ -1,10 +1,35 @@
+const detail = document.getElementById("details");
+const img = document.getElementById("picture");
 const setbook = () => {
-  console.log(localStorage.getItem("current"));
-  localStorage.getItem("current") ? bookApp() : bookApp("error");
-};
-const bookApp = (err) => {
-  const detail = document.querySelector("#details");
-  err
-    ? (detail.innerText = "Error")
-    : (detail.innerHTML = localStorage.getItem("current"));
-};
+  const current = localStorage.getItem("active");
+  const getbooks = async () => {
+  const response = await fetch("scripts/books.json");
+  const booklist = await response.json();
+  const updata = () => {
+    console.log(current);
+    console.log(booklist[0].name);
+    let imgform = `
+    <div class="picture"> <img src="${booklist[current].picture}" class="bimg"></div>`
+    img.innerHTML += imgform;
+    let cardformat = `
+      <div class="card">
+          <strong><center><h1>${booklist[current].name}</h1></center></strong>
+          <strong><p class="head">Author:</strong> ${booklist[current].author}<br>
+          <strong>Edition:</strong> ${booklist[current].edition}<br>
+          <strong>Price:</strong> ${booklist[current].price}<br>
+          <strong>Posted By:</strong> ${booklist[current].by}<br>
+          <strong>Contact:</strong> ${booklist[current].contact}</p><br>
+          <p></p>
+      </div>`;
+      detail.innerHTML += cardformat;
+      };
+    updata();
+}
+getbooks();
+}
+//SEARCHING FUNCTIONALITY
+const query = document.querySelector("#searching");
+query.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(query.value);
+})
